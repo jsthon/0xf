34b2758e9120f0b387fb3c4624ce79b733f41102
jsthon <jsthon@live.com>
2025-05-01 21:39:26 +0800

    chore(debug): add next bundle analyzer

--- next.config.ts
+++ next.config.ts
@@ -3,8 +3,14 @@ import createNextIntlPlugin from "next-intl/plugin";
 
 const withNextIntl = createNextIntlPlugin();
 
+// only load bundle analyzer when needed
+const withBundleAnalyzer =
+  process.env.ANALYZE === "true"
+    ? require("@next/bundle-analyzer")({ enabled: true })
+    : (config: NextConfig) => config;
+
 const nextConfig: NextConfig = {
   /* config options here */
 };
 
-export default withNextIntl(nextConfig);
+export default withBundleAnalyzer(withNextIntl(nextConfig));
