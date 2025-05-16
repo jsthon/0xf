758209f7ade15cea533e850c8b27da642de2d34d
jsthon <jsthon@live.com>
2025-05-16 22:57:30 +0800

    feat(tools): add color converter tool

---
+++ app/[locale]/(pages)/(sidebar)/(tools)/color-converter/layout.tsx
@@ -0,0 +1,28 @@
+import { Metadata } from "next";
+import { Locale } from "next-intl";
+import { getTranslations } from "next-intl/server";
+
+export async function generateMetadata({
+  params,
+}: Readonly<{
+  params: Promise<{ locale: Locale }>;
+}>): Promise<Metadata> {
+  const { locale } = await params;
+  const t = await getTranslations({
+    locale,
+    namespace: "ColorConverterPage.Meta",
+  });
+
+  return {
+    title: t("Title"),
+    description: t("Description"),
+  };
+}
+
+export default function ColorConverterLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return <>{children}</>;
+}
